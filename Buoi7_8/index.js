6862e7975ffb9bd4b0ee6dc19113bdc178e749d7
KhaiNguyen1608 <nguyenkhai.huflit@gmail.com>
2022-07-28 14:11:19 +0700

    Bai Tap 7-8

--- Buoi7_8/index.js
+++ Buoi7_8/index.js
@@ -16,7 +16,10 @@ function addNumber() {
 
 
 function baiTap1() {
-     var arr =  document.getElementById("resultNumber").innerHTML.split(',');
+     var arr =  document.getElementById("resultNumber").innerHTML.split(',').map(Number);
+     if(arr[0] == "") {
+          return;
+     }
      var i = 0;
      var sum = 0;
      for(i = 0; i <= arr.length - 1; i++)
@@ -30,7 +33,10 @@ function baiTap1() {
 
 
 function baiTap2() {
-     var arr =  document.getElementById("resultNumber").innerHTML.split(',');
+     var arr =  document.getElementById("resultNumber").innerHTML.split(',').map(Number);
+     if(arr[0] == "") {
+          return;
+     }
      var i = 0;
      var count = 0;
      for(i = 0; i <= arr.length - 1; i++)
@@ -44,61 +50,80 @@ function baiTap2() {
 
 
 function baiTap3() { 
-     var min = Math.min(arr);
-     console.log(min);
+     var arr = document.getElementById("resultNumber").innerHTML.split(',').map(Number);
+     if(arr[0] == "") {
+          return;
+     }
+    
+     var min = Math.min(...arr);
+     document.getElementById("resultBT3").innerHTML = min;
 }
 
 
 function baiTap4() { 
      var i = 0;
+     var arr = document.getElementById("resultNumber").innerHTML.split(',').map(Number);
      var newArr = [];
      for(i = 0; i < arr.length - 1; i++)
      {
-          if(i >= 0) {
+          if(arr[i] >= 0) {
                newArr.push(arr[i]);
           }
      }
-     var min = Math.min(newArr);
-     console.log(min);
+     var min = Math.min(...newArr);
+     document.getElementById("resultBT4").innerHTML = min;
 }
 
 function baiTap5() { 
      var i = 0;
+     var arr = document.getElementById("resultNumber").innerHTML.split(',').map(Number);
      var newArr = [];
      for(i = 0; i < arr.length - 1; i++)
      {
-          if(i%2 === 0) {
+          if(arr[i] % 2 === 0) {
                newArr.push(arr[i]);
           }
      }
      var tmp = newArr[newArr.length-1]
-     console.log(tmp);
+     document.getElementById("resultBT5").innerHTML = tmp;
 }
 
 
 function baiTap6() { 
+     var arr = document.getElementById("resultNumber").innerHTML.split(',').map(Number);
+     var i = document.getElementById("pos1").value*1;
+     var k = document.getElementById("pos2").value*1;
      var tmpValue = 0;
      if(arr[i] != undefined && arr[k] != undefined) {
           tmpValue = arr[i];
           arr[i] = arr[k];
           arr[k] = tmpValue;
      }
+     document.getElementById("resultBT6").innerHTML = arr;
 }
 
 function baiTap7() { 
-    arr.sort((a,b) => a - b);
+     var arr = document.getElementById("resultNumber").innerHTML.split(',').map(Number);
+     arr.sort((a,b) => a - b);
+     document.getElementById("resultBT7").innerHTML = arr;
 }
 
 function baiTap8() { 
-     var tmpValue = 0;
-     if(arr[i] != undefined && arr[k] != undefined) {
-          tmpValue = arr[i];
-          arr[i] = arr[k];
-          arr[k] = tmpValue;
+     var arr = document.getElementById("resultNumber").innerHTML.split(',').map(Number);
+     var i = 0;
+     var result = '';
+     for(i = 0; i < arr.length - 1; i++)
+     {
+          if(arr[i] >= 2 && arr[i] % 3 !== 0 && arr[i] % 5 !== 0 && arr[i] % 7 !== 0) {
+              result = arr[i];
+              break;
+          }
      }
+     document.getElementById("resultBT8").innerHTML = result;
 }
 
 function baiTap9() {
+     var arr = document.getElementById("resultNumber").innerHTML.split(',').map(Number);
      var i = 0;
      var count = 0;
      for(i = 0; i < arr.length - 1; i++)
@@ -107,11 +132,12 @@ function baiTap9() {
                count++
           }
      }
-     console.log(count);
+     document.getElementById("resultBT9").innerHTML = count;
 }
 
 
 function baiTap10() {
+     var arr = document.getElementById("resultNumber").innerHTML.split(',').map(Number);
      var i = 0;
      var countPos = 0;
      var countNeg = 0;
@@ -129,13 +155,16 @@ function baiTap10() {
      
      if(countNeg == countPos)
      {
-          console.log("Số dương = Số âm")
+          document.getElementById("resultBT10").innerHTML = "Số dương = Số âm";
+          
      }
      else if(countNeg > countPos) {
-          console.log("Số dương < Số âm")
+          document.getElementById("resultBT10").innerHTML = "Số dương < Số âm";
+         
      }
      else if(countNeg < countPos)
      {
-          console.log("Số dương > Số âm")
+          document.getElementById("resultBT10").innerHTML = "Số dương > Số âm";
+         
      }
 }
